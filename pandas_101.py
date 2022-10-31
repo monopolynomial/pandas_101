b6affd6712d2da6481f877693e94e4825a5967b6
monopolynomial <19833580+monopolynomial@users.noreply.github.com>
2022-10-31 22:52:52 +0000

    Update pandas_101.py

--- pandas_101.py
+++ pandas_101.py
@@ -4,3 +4,7 @@ data=pd.DataFrame({'x1':[1,2,1],'x2':[4,5,6]})
 data.sort_values('x1')
 data.groupby('x1').sum()
 data.sum(axis=1)# sum of each row
+# great! any function can be used 😎
+data.max(axis=1)
+data.max(axis=0)
+
